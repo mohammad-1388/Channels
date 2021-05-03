0e45e71dcf88408b66979d5391f6babbbb9056ab
mohammad-1388 <mohammad.eydi1420@gmail.com>
2021-05-03 10:44:05 +0430

    helllop

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,44 +1,48 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
-from tkinter import *
-
-
-
+from asgiref.sync import async_to_sync
 class ChatConsumer(WebsocketConsumer):
-    """
-        This Consumer for Chat Model
-    """
-
+    
     def connect(self):
+        
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'caht_{self.room_name}'
-
-        self.channel_layer.group_add(
+        self.room_group_name = f'chat_{self.room_name}'
+        
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
-        )
+        )    
         self.accept()
 
-    def disconnect(self, code):
-        self.channel_layer.group_discard(
+    
+    
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+
             self.room_group_name,
             self.channel_name
+            
         )
-
+    
     def receive(self, text_data):
         text_data_dict = json.loads(text_data)
         message = text_data_dict['message']
-
-
-        self.channel_layer.group_send (
+        
+        
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type': None,
-                'message': message,
+                'type': 'chat_message',
+                'message': message
+                
             }
+            
+            
         )
-    def chat_message(self, event):
 
+    
+    def chat_message(self, event):
+        message = event['message']
         self.send(text_data=json.dumps({
             'message': message
         }))
